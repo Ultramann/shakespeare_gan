0d67a957bd69612d51ea72689c726e8f28381024
Ultramann <cary.goltermann@gmail.com>
2016-08-06 11:34:59 -0700

    Add sentence handling class.
    Largest part of this is that the class has a method to get a new
    generator that yields batches of specified size of random
    sentences.

--- data_processing.py
+++ data_processing.py
@@ -3,10 +3,6 @@ import re
 import numpy as np
 
 
-SENTENCE_START_TOKEN = '%'
-SENTENCE_END_TOKEN = '#'
-
-
 def load_file(file_path):
     '''Helper function for loading contents at file path.'''
 
@@ -14,25 +10,70 @@ def load_file(file_path):
         return f.read()
 
 
-def load_shakespeare_sentences(text_folder='clean_text/'):
-    '''Gotta get all those sentences in a list.'''
+class ShakespeareSentences:
+    '''
+    Class to simplify getting non-repeating, random sets of Shakespeare sentences.
+    '''
+    def __init__(self, text_folder='clean_text/',
+                 start_token='%', end_token='#'):
 
-    def sentenciate(text_path):
-        '''Helper function for making sentences from text in file at text_path.'''
-        text = load_file(os.path.join(text_folder, text_path))
-        sentences_n_punct = re.split(r'(\.|\?|\!)', text)
-        full_sentences = []
-        for i in range(0, len(sentences_n_punct)-1, 2):
-            sentence = sentences_n_punct[i] + sentences_n_punct[i+1]
-            full_sentences.append(SENTENCE_START_TOKEN + 
-                                  sentence.strip() +
-                                  SENTENCE_END_TOKEN)
-        return full_sentences
+        self.start_token = start_token
+        self.end_token = end_token
+        self.chars = set((start_token, end_token))
+        self.sentences = self._get_sentences(text_folder)
+        self.total_sentences = len(self.sentences)
 
-    text_files = os.listdir(text_folder)
-    sentences = [sentence for text_path in text_files 
-                          for sentence in sentenciate(text_path)]
-    return sentences
+    def __repr__(self):
+        sentence_message = 'There are a total of {} Shakespeare sentences.'
+        return sentence_message.format(self.total_sentences)
+
+    def _get_sentences(self, text_folder):
+
+        def sentenciate(text_path):
+            '''
+            Helper function for making sentences from text in 
+            file at text_path.
+            '''
+            text = load_file(os.path.join(text_folder, text_path))
+            sentences_n_punct = re.split(r'(\.|\?|\!)', text)
+            full_sentences = []
+            for i in range(0, len(sentences_n_punct)-1, 2):
+                sentence = sentences_n_punct[i] + sentences_n_punct[i+1]
+                self.chars.update(set(sentence))
+                full_sentences.append(self.start_token + 
+                                      sentence.strip() +
+                                      self.end_token)
+            return full_sentences
+
+        text_files = os.listdir(text_folder)
+        sentences = np.array([sentence for text_path in text_files 
+                                       for sentence in sentenciate(text_path)])
+        return sentences
+
+    def get_batch_generator(self, batch_size, epoch_size):
+        '''
+        Method to get generator that yields batches of non-repeating
+        Shakespeare sentences.
+
+        Args:
+            batch_size (int):     Number of sentences per batch
+            epoch_size (int/str): How many sentences to go through in an epoch
+        '''
+
+        def sentence_batch_gen(rand_sentences):
+            '''Generator that yields batches of random sentences.'''
+            for i in range(0, len(rand_sentences), batch_size):
+                sentence_batch = rand_sentences[i:i+batch_size]
+                yield sentence_batch
+
+        if epoch_size == 'full':
+            epoch_size = self.total_sentences
+
+        idxs = np.arange(self.total_sentences)
+        np.random.shuffle(idxs)
+        random_idxs = idxs[:epoch_size]
+        random_sentences = self.sentences[random_idxs]
+        return sentence_batch_gen(random_sentences)
 
         
 class CharacterCoder:
@@ -41,11 +82,10 @@ class CharacterCoder:
     def __init__(self, chars):
         '''
         Args:
-            chars (iterable): All of the characters that the CharacterCoder will
-                              be able to (en/de)code.
+            chars (set): All of the characters that the CharacterCoder will
+                         be able to (en/de)code
         '''
-        self.chars = sorted(set(chars) | set((SENTENCE_START_TOKEN, 
-                                              SENTENCE_END_TOKEN)))
+        self.chars = sorted(chars)
         self.char_hash = {c: i for i, c in enumerate(self.chars)}
         self.idx_hash = {i: c for i, c in enumerate(self.chars)}
         self.vector_len = len(self.chars)
@@ -53,11 +93,11 @@ class CharacterCoder:
     def encode(self, chars):
         '''
         Args:
-            chars (str): Characters to encoded to one-hot vectors.
+            chars (str): Characters to encoded to one-hot vectors
 
         Returns:
             ndarray: 2D array with one-hot vectors corresponding to the character
-                    at each position.
+                     at each position
         '''
         char_vectors = np.zeros((len(chars), self.vector_len))
         for i, char in enumerate(chars):
@@ -67,10 +107,10 @@ class CharacterCoder:
     def decode(self, char_vectors):
         '''
         Args:
-            char_vectors (ndarray): 2D array of corresponding to the characters.
+            char_vectors (ndarray): 2D array of corresponding to the characters
 
         Returns:
-            str: Decoded string.
+            str: Decoded string
                
         '''
         char_idxs = char_vectors.argmax(axis=-1)
--- modeling.py
+++ modeling.py
@@ -25,10 +25,28 @@ class ModelTrainer:
     '''Class for handling the specific needs involving training a GAN.'''
 
     def __init__(self, sentences, generator, discriminator, gen_dis):
+        '''
+        Args:
+            sentences (ShakespeareSentences)
+            generator (Model):     Pre-compiled Keras model to generate character
+                                   sequences from noise.
+            discriminator (Model): Pre-compiled Keras model to distinguish between
+                                   character sequences that are real and those
+                                   faked by the generator.
+            gen_dis (Model):       Keras model comprised of stacking the generator
+                                   and discriminator for training end to end.
+        '''
         self.sentences = sentences
         self.gen = generator
         self.dis = discriminator
         self.gen_dis = gen_dis
 
-    def train(self, batch_size, epochs):
+    def train(self, batch_size, nb_epochs, epoch_size='full'):
+        for epoch in range(nb_epochs):
+            sentences_batch_gen = self.sentences.get_batch_generator(batch_size,
+                                                                     epoch_size)
+            for batch in sentences_batch_gen:
+                self._train_on_batch(batch)
+
+    def _train_on_batch(self, batch):
         pass
